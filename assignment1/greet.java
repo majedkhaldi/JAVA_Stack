793924114827bae8d0dca2096659328bc50e4e9a
Majed Alkhaldi <majed_khaldi@hotmail.com>
2024-06-10 13:01:58 +0300

    first assignment ready

---
+++ assignment1/greet.java
@@ -0,0 +1,10 @@
+public class greet {
+
+    public static void main(String[] args) {
+        String name = "axsos";
+        int age = 20;
+        String home = "ramallah";
+        String result = String.format("My Name is %s\n my age is %d \n i am from %s", name,age,home);
+        System.out.println(result);
+    }
+}
